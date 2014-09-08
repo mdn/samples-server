2252d541947d965e597bbf376d2be72f84d13e77
Eric Shepherd <sheppy@sheppyware.net>
2014-09-08 16:20:24 -0400

    Update manifest to include more

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Handle setup for the MDN sample server.
+#
+# For now, this isn't doing anything to speak of. It will eventually.
+
+FS=$STACKATO_FILESYSTEM
+
+echo "MDN sample server now ready."
