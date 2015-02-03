d7a3d3fd3f0aafc2bd4f13bc2f8fd4a7925b2697
Eric Shepherd <sheppy@sheppyware.net>
2015-02-03 16:24:58 -0500

    user-data script should be ready to go now

--- user-data.sh
+++ user-data.sh
@@ -6,6 +6,9 @@
 # This lets us do things like ensure that specific software is installed,
 # as well as to spin-up needed background tasks.
 #
+# This script is based on this tutorial on the AWS docs site:
+#    http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-LAMP.html
+#
 
 # Update existing software
 
@@ -13,17 +16,31 @@ yum update -y
 
 # Install packages needed for Web server support
 
-yum groupinstall "Web Server" "MySQL Database" "PHP Support"'
-yum install -y php-mysql
+yum groupinstall -y "Web Server" "PHP Support"
+yum install -y nodejs
+yum install -y npm
+
+# Now install needed Node packages
+
+npm install ws
 
 # Start up the Web server
 
-service httpd start
-chkconfig httpd on
+systemctl start  httpd.service
+systemctl enable httpd.service
 
 # Create a group for the Web content and add the default user to it
 
+groupadd www
 usermod -a -G www ec2-user
+
+# Fetch the site content from github and install it
+
+wget git@github.com:a2sheppy/mdn-samples.git /tmp/mdn-samples.git
+unzip /tmp/mdn-samples-master.zip 
+rm -r /var/www
+mv /tmp/mdn-samples-master /var/www/html
+
 chown -R root:www /var/www
 chmod 2775 /var/www
 
@@ -31,10 +48,20 @@ chmod 2775 /var/www
 
 find /var/www -type d -exec chmod 2775 {} +
 
-# Update permissions of Web content: Folders
+# Update permissions of Web content: Files
 
 find /var/www -type f -exec chmod 0664 {} +
 
+# Ensure that sshd is set up right and start it
+
+chmod 711 /var/empty/sshd
+chmod 600 /etc/ssh/*_key
+systemctl start sshd.service
+
 # Start spinning up Sample Server stuff here
 
-echo "<?php phpinfo(); ?>" /var/www/html/phpinfo.php
+#ls -l /var/empty > /var/www/html/output1.txt
+#systemctl status sshd.service > /var/www/html/ssh-status1.txt
+#ls -l /etc/ssh > /var/www/html/ssh-dir-listing.txt
+#systemctl start sshd.service &> /var/www/html/ssh-start.txt
+#systemctl status sshd.service > /var/www/html/ssh-status2.txt
