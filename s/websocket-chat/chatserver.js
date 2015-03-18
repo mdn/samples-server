8c12bb0d34233354c62e1c92839793569e1b38cf
Eric Shepherd <sheppy@sheppyware.net>
2015-03-18 15:22:04 -0400

    More logging, cleaned up stuff
    Cleaned up some stuff and added a couple of logging lines.

--- s/websocket-chat/chatserver.js
+++ s/websocket-chat/chatserver.js
@@ -12,9 +12,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var WebSocketServer = require('websocket/lib/WebSocketServer');
-
-var port = process.env.stackato_harbor_websocket_chat_port;
+var WebSocketServer = require('websocket').server;
 
 var connectionArray = [];
 var nextID = Date.now();
@@ -30,10 +28,18 @@ server.listen(6502, function() {
     console.log((new Date()) + " Server is listening on port 6502");
 });
 
+// Create the WebSocket server
+
 var wsServer = new WebSocketServer({
     httpServer: server,
-    autoAcceptConnections: true
+    autoAcceptConnections: true // You should use false here!
 });
+
+function originIsAllowed(origin) {
+  // This is where you put code to ensure the connection should
+  // be accepted. Return false if it shouldn't be.
+  return true;
+}
 
 function isUsernameUnique(name) {
   var isUnique = true;
@@ -88,78 +94,94 @@ function sendUserListToAll() {
   }
 }
 
+console.log("Adding connection listener...");
+
 wsServer.on('connect', function(connection) {
-    console.log((new Date()) + " Connection accepted.");
-    connectionArray.push(connection);
+//  if (!originIsAllowed(connection.origin)) {
+//    request.reject();
+//    console.log((new Date()) + "Connection from " + connection.origin + " rejected.");
+//    return;
+//  }
+  
+  console.log((new Date()) + " Connection accepted.");
+  connectionArray.push(connection);
 
-    // Send the new client its token; it will
-    // respond with its login username.
+  // Send the new client its token; it will
+  // respond with its login username.
 
-    connection.clientID = nextID;
-    nextID++;
+  connection.clientID = nextID;
+  nextID++;
 
-    var msg = {
-      type: "id",
-      id: connection.clientID
-    };
-    connection.sendUTF(JSON.stringify(msg));
+  var msg = {
+    type: "id",
+    id: connection.clientID
+  };
+  connection.sendUTF(JSON.stringify(msg));
 
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log("Received Message: " + message.utf8Data);
+  // Handle the "message" event received over WebSocket. This
+  // is a message sent by a client, and may be text to share with
+  // other users or a command to the server.
 
-            // Process messages
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          console.log("Received Message: " + message.utf8Data);
 
-            var sendToClients = true;
-            msg = JSON.parse(message.utf8Data);
-            var connect = getConnectionForID(msg.id);
+          // Process messages
 
-            switch(msg.type) {
-              case "message":
-                msg.name = connect.username;
-                msg.text = msg.text.replace(/(<([^>]+)>)/ig,"");
-                break;
-              case "username":
-                var nameChanged = false;
-                var origName = msg.name;
+          var sendToClients = true;
+          msg = JSON.parse(message.utf8Data);
+          var connect = getConnectionForID(msg.id);
 
-                while (!isUsernameUnique(msg.name)) {
-                  msg.name = origName + appendToMakeUnique;
-                  appendToMakeUnique++;
-                  nameChanged = true;
-                }
+          switch(msg.type) {
+            case "message":
+              msg.name = connect.username;
+              msg.text = msg.text.replace(/(<([^>]+)>)/ig,"");
+              break;
+            case "username":
+              var nameChanged = false;
+              var origName = msg.name;
 
-                if (nameChanged) {
-                  var changeMsg = {
-                    id: msg.id,
-                    type: "rejectusername",
-                    name: msg.name
-                  };
-                  connect.sendUTF(JSON.stringify(changeMsg));
-                }
-
-                connect.username = msg.name;
-                sendUserListToAll();
-                break;
-            }
-
-            // Convert the message back to JSON and send it.
-
-            if (sendToClients) {
-              var msgString = JSON.stringify(msg);
-              var i;
-
-              for (i=0; i<connectionArray.length; i++) {
-                connectionArray[i].sendUTF(msgString);
+              while (!isUsernameUnique(msg.name)) {
+                msg.name = origName + appendToMakeUnique;
+                appendToMakeUnique++;
+                nameChanged = true;
               }
+
+              if (nameChanged) {
+                var changeMsg = {
+                  id: msg.id,
+                  type: "rejectusername",
+                  name: msg.name
+                };
+                connect.sendUTF(JSON.stringify(changeMsg));
+              }
+
+              connect.username = msg.name;
+              sendUserListToAll();
+              break;
+          }
+
+          // Convert the message back to JSON and send it.
+
+          if (sendToClients) {
+            var msgString = JSON.stringify(msg);
+            var i;
+
+            for (i=0; i<connectionArray.length; i++) {
+              connectionArray[i].sendUTF(msgString);
             }
-        }
+          }
+      }
+  });
+  
+  // Handle the WebSocket "close" event; this means a user has logged off
+  // or has been disconnected.
+  
+  connection.on('close', function(connection) {
+    connectionArray = connectionArray.filter(function(el, idx, ar) {
+      return el.connected;
     });
-    connection.on('close', function(connection) {
-      connectionArray = connectionArray.filter(function(el, idx, ar) {
-              return el.connected;
-      });
-      sendUserListToAll();  // Update the user lists
-      console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
-    });
+    sendUserListToAll();  // Update the user lists
+    console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+  });
 });
