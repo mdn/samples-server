54256a256689eb86b6e4e86201761fb31a24d8ac
Eric Shepherd <sheppy@sheppyware.net>
2015-06-15 17:43:06 -0400

    A bit more of the content is in
    I’ve got more content and a bit more code done but am evaluating
    rewriting bits of my code to use promises since that’s apparently
    highly recommended best practice.

--- s/webrtc-simple-datachannel/main.js
+++ s/webrtc-simple-datachannel/main.js
@@ -7,13 +7,27 @@
   // Set things up, connect event listeners, etc.
   
   function startup() {
+    var connectButton = document.getElementById('connectButton');
+    var disconnectButton = document.getElementById('disconnectButton');
     var sendButton = document.getElementById('sendButton');
     
+    // Set event listeners for user interface widgets
+    
+    connectButton.addEventListener('click', connectToPeer, false);
     sendButton.addEventListener('click', sendMessage, false);
   }
   
-  function sendMessage(e) {
-    console.log("Sending message: " + e);
+  // Connect to the remote peer.
+  
+  function connectToPeer() {
+    
+  }
+  
+  // Handles clicks on the "Send" button by transmitting
+  // a message to the remote peer.
+  
+  function sendMessage() {
+    console.log("Sending message");
   }
   
   // Set up an event listener which will run the startup
