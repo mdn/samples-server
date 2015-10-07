4dc858b38a59976adaed64b2df8f586a0d9e95e3
Eric Shepherd <sheppy@sheppyware.net>
2015-10-06 21:51:12 -0400

    New WebRTC signaling sample WIP
    Adding the initial code for the WebRTC signaling server sample. This is
    based on the text chat server that uses WebSockets, but adds support
    for inviting a user into two-way video chat. The signaling support code
    has been added to the chatserver.js file, and early changes made to the
    HTML and CSS, but signaling doesn’t yet take place. I just wanted to
    get the code checked in for tracking work going forward.

---
+++ s/webrtc-from-chat/chatserver.js
@@ -0,0 +1,241 @@
+//#!/usr/bin/env node
+
+//
+// WebSocket chat server
+// Implemented using Node.js
+//
+// Requires the websocket module.
+//
+
+"use strict";
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var websocket = require('websocket').server;
+
+// Used for managing the text chat user list.
+
+var connectionArray = [];
+var nextID = Date.now();
+var appendToMakeUnique = 1;
+
+// Stuff related to WebRTC video chats in progress.
+
+var webrtc_chats = {};
+
+// Our HTTP server does nothing but service WebSocket
+// connections, so every request just returns 404. Real Web
+// requests are handled by the main server on the box.
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + " Received request for " + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(6503, function() {
+    console.log((new Date()) + " Server is listening on port 6503");
+});
+
+// Create the WebSocket server
+
+var wsServer = new websocket({
+    httpServer: server,
+    autoAcceptConnections: true // You should use false here!
+});
+
+function originIsAllowed(origin) {
+  // This is where you put code to ensure the connection should
+  // be accepted. Return false if it shouldn't be.
+  return true;
+}
+
+function isUsernameUnique(name) {
+  var isUnique = true;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].username === name) {
+      isUnique = false;
+      break;
+    }
+  }
+  return isUnique;
+}
+
+function getConnectionForID(id) {
+  var connect = null;
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    if (connectionArray[i].clientID === id) {
+      connect = connectionArray[i];
+      break;
+    }
+  }
+
+  return connect;
+}
+
+function makeUserListMessage() {
+  var userListMsg = {
+    type: "userlist",
+    users: []
+  };
+  var i;
+
+  // Add the users to the list
+
+  for (i=0; i<connectionArray.length; i++) {
+    userListMsg.users.push(connectionArray[i].username);
+  }
+
+  return userListMsg;
+}
+
+// Sends a "userlist" message to all chat members. This is a cheesy way
+// to ensure that every join/drop is reflected everywhere. It would be more
+// efficient to send simple join/drop messages to each user, but this is
+// good enough for this simple example.
+function sendUserListToAll() {
+  var userListMsg = makeUserListMessage();
+  var userListMsgStr = JSON.stringify(userListMsg);
+  var i;
+
+  for (i=0; i<connectionArray.length; i++) {
+    connectionArray[i].sendUTF(userListMsgStr);
+  }
+}
+
+wsServer.on('connect', function(connection) {
+//  if (!originIsAllowed(connection.origin)) {
+//    request.reject();
+//    console.log((new Date()) + "Connection from " + connection.origin + " rejected.");
+//    return;
+//  }
+  
+  console.log((new Date()) + " Connection accepted.");
+  connectionArray.push(connection);
+
+  // Send the new client its token; it will
+  // respond with its login username.
+
+  connection.clientID = nextID;
+  nextID++;
+
+  var msg = {
+    type: "id",
+    id: connection.clientID
+  };
+  connection.sendUTF(JSON.stringify(msg));
+
+  // Handle the "message" event received over WebSocket. This
+  // is a message sent by a client, and may be text to share with
+  // other users or a command to the server.
+
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          console.log("Received Message: " + message.utf8Data);
+
+          // Process incoming data.
+
+          var sendToClients = true;
+          msg = JSON.parse(message.utf8Data);
+          var connect = getConnectionForID(msg.id);
+
+          // Take a look at the incoming object and act on it based
+          // on its type.
+
+          switch(msg.type) {
+            // Public, textual message
+            case "message":
+              msg.name = connect.username;
+              msg.text = msg.text.replace(/(<([^>]+)>)/ig,"");
+              break;
+            
+            // Username change
+            case "username":
+              var nameChanged = false;
+              var origName = msg.name;
+
+              // Ensure the name is unique by appending a number to it
+              // if it's not; keep trying that until it works.
+              while (!isUsernameUnique(msg.name)) {
+                msg.name = origName + appendToMakeUnique;
+                appendToMakeUnique++;
+                nameChanged = true;
+              }
+
+              if (nameChanged) {
+                var changeMsg = {
+                  id: msg.id,
+                  type: "rejectusername",
+                  name: msg.name
+                };
+                connect.sendUTF(JSON.stringify(changeMsg));
+              }
+
+              // Update the current username and send it along to
+              // all users.
+              connect.username = msg.name;
+              sendUserListToAll();
+              sendToClients = false;  // We already sent the proper responses
+              break;
+              
+            // Join (or create) a video call channel, identified by a callID
+            // which is created by the channel creator and sent to the
+            // callee.
+            case "video":
+              if (msg.callID !== undefined) {
+                if (webrtc_chats[msg.callID] === undefined) { // New channel?
+                  webrtc_chats[msg.callID] = {};
+                }
+                
+                // Whether it's new or not, we now record that this call
+                // is associated with this user's ID.
+                
+                webrtc_chats[msg.callID][connect.id] = true;
+              }
+              sendToClients = false;  // Nothing to send
+              break;
+          }
+          
+          // Convert the revised message back to JSON and send it out
+          // to all clients, if doing so is necessary.
+
+          if (sendToClients) {
+            var msgString = JSON.stringify(msg);
+            var i;
+
+            for (i=0; i<connectionArray.length; i++) {
+              connectionArray[i].sendUTF(msgString);
+            }
+          }
+      }
+  });
+  
+  // Handle the WebSocket "close" event; this means a user has logged off
+  // or has been disconnected.
+  
+  connection.on('close', function(connection) {
+    // First, remove the connection from the list of connections.
+    
+    connectionArray = connectionArray.filter(function(el, idx, ar) {
+      return el.connected;
+    });
+    
+    // Now find any WebRTC connections the user was connected to and
+    // shut them down.
+        
+    Object.keys(webrtc_chats).forEach(function(callID) {
+      Object.keys(webrtc_chats[callID]).forEach(function(connectID) {
+        if (connectID == connection.id) {
+          delete webrtc_chats[callID][connectID];
+        }
+      });
+    });
+    sendUserListToAll();  // Update the user lists
+    console.log((new Date()) + " Peer " + connection.remoteAddress + " disconnected.");
+  });
+});
---
+++ s/webrtc-from-chat/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npm install websocket
+node chatserver.js
