49c605fbda926a2dce9955b362233eef673e6090
Eric Shepherd <sheppy@sheppyware.net>
2015-11-16 15:06:29 -0500

    New sample: signaling using WebSocket messages on a chat server, with two-way video calling.
    New: tool to extract a function from a JavaScript file on the sample server.
    Not finished yet. Do not use.

---
+++ tools/extract-function/extract-function.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Include required node modules
+
+var express = require("express");
+var fnExtractor = require("function-extractor");
+
+var app = express();
+
